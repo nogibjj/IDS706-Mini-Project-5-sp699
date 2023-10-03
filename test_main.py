db649d3276d6c27140772d6f275dd5449b093d11
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-03 21:39:25 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -17,20 +17,15 @@ def test_load_file():
     test_csv_file = "test.csv"
     with open(test_csv_file, "w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([
-            "PassengerId", "Survived", "Pclass", "Name", "Sex",
-            "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"
-        ])
+        writer.writerow(["id", "survived", "pclass", "sex", "age"])
         # 데이터 행
-        writer.writerow([
-            893, 0, 3, "Braund, Mr. Owen Harris", "male", 22, 1, 0, "A/5 21171", 7.25, "", "S"
-        ])
+        writer.writerow([13, 0, 3, "male", 32])
     
     # 테스트용 DB 파일 경로
-    test_db_file = "test_db.db"
+    test_db_file = "test_subsetDB.db"
     
     # CSV 파일을 DB로 로드
-    load_file(test_csv_file, test_db_file, "test_table")
+    load_file(test_csv_file, test_db_file, "test_subset")
     
     # DB에서 데이터 확인
     conn = sqlite3.connect(test_db_file)
@@ -43,24 +38,24 @@ def test_load_file():
     assert count == 1
 
 def test_create_CRUD():
-    data = (893, 1, 3, "John, Mr. Doe", "male", 25, 0, 0, "12345", 7.25, "", "S")
+    data = (14, 1, 3, "female", 25)
     create_CRUD(data)
 
     # Connect to the database and check if the data was inserted
-    conn = sqlite3.connect("titanic_passengersDB")
+    conn = sqlite3.connect("subsetDB")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM titanic WHERE PassengerId = 893")
+    cursor.execute("SELECT * FROM titanic WHERE id = 14")
     result = cursor.fetchone()
 
     assert result == data
 
     # Clean up by deleting the inserted data
-    cursor.execute("DELETE FROM titanic WHERE PassengerId = 893")
+    cursor.execute("DELETE FROM titanic WHERE id = 14")
     conn.commit()
     conn.close()
 
 def test_read_CRUD():
-    test_data = (1, 0, 3, "Braund, Mr. Owen Harris", "male", 22, 1, 0, "A/5 21171", 7.25, "", "S")
+    test_data = (1, 0, 3, "male", 22)
     create_CRUD(test_data)
     result = read_CRUD()
 
@@ -72,32 +67,31 @@ def test_read_CRUD():
 
 def test_update_CRUD():
     # Insert test data
-    test_data = (892, 1, 1, "Rose, Miss. DeWitt Bukater", "female", 19, 0, 1, 12, 100, 2, "Q")
+    test_data = (13, 1, 1, "female", 19)
     create_CRUD(test_data)
 
     # Call the update_CRUD function to update a specific column
-    record_id = 892
+    record_id = 13
     column_name = "Age"
     new_value = 18
 
     update_CRUD(record_id, column_name, new_value)
     new_database = read_CRUD()
 
-    assert new_database == (892, 1, 1, "Rose, Miss. DeWitt Bukater", "female", 18, 0, 1, 12, 100, 2, "Q")
+    assert new_database == (13, 1, 1, "female", 18)
 
 def test_delete_CRUD():
     # Insert test data
-    test_data = (893, 1, 3, "Bob, Mr. Johnson", "male", 28, 0, 0, "67890", 9.0, "", "S")
-    create_CRUD(test_data)
+    test_data = (13, 1, 1, "female", 18)
 
     # Call the delete_CRUD function to delete a record
-    record_id = 893
+    record_id = 13
     delete_CRUD(record_id)
 
     # Connect to the database and check if the record was deleted
-    conn = sqlite3.connect("titanic_passengersDB")
+    conn = sqlite3.connect("subsetDB")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM titanic WHERE PassengerId = ?", (record_id,))
+    cursor.execute("SELECT * FROM titanic WHERE id = ?", (record_id,))
     result = cursor.fetchone()
 
     # Check if the result is None, indicating the record was deleted

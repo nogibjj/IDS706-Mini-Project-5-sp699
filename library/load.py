31d283635ebcdeb9af84eda1a4f29bab0ce79773
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-04 13:32:05 -0400

    Update load.py

--- library/load.py
+++ library/load.py
@@ -1,18 +1,17 @@
 # Transform .csv file to .db (SQLite) file
 import sqlite3
 
-
 # Load the .csv file and transform it for SQLite
 def load_database():
     conn = sqlite3.connect("subsetDB.db")
-        
+
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM subset")
     results = cursor.fetchall()
 
-    # age 열의 데이터 형식을 정수로 변경
-    cursor.execute("PRAGMA foreign_keys = 0")  # 외래 키 비활성화
-    cursor.execute("ALTER TABLE subset RENAME TO subset_old")  # 현재 테이블을 이름 변경
+    # Change the data type of the 'age' column to integer
+    cursor.execute("PRAGMA foreign_keys = 0")  # Disable foreign keys
+    cursor.execute("ALTER TABLE subset RENAME TO subset_old")  # Rename the current table
     cursor.execute("""
     CREATE TABLE subset (
         id INTEGER PRIMARY KEY,
@@ -22,17 +21,17 @@ def load_database():
         age INTEGER
         )
     """)
-    # 데이터 복사
+    # Copy the data
     cursor.execute("""
         INSERT INTO subset (id, survived, pclass, sex, age)
         SELECT id, survived, pclass, sex, CAST(age AS INTEGER) FROM subset_old
     """)
 
-    cursor.execute("DROP TABLE subset_old")  # 이전 테이블 삭제
-    cursor.execute("PRAGMA foreign_keys = 1")  # 외래 키 다시 활성화
+    cursor.execute("DROP TABLE subset_old")  # Delete the old table
+    cursor.execute("PRAGMA foreign_keys = 1")  # Re-enable foreign keys
 
-    # 변경사항 커밋
-    conn.commit()  
+    # Commit the changes
+    conn.commit()
     conn.close()
-        
+
     return results

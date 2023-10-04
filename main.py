1ad55d084a5d46d3e8b4895e62a9aec95a9c2844
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-04 10:58:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,14 @@ from library.query import create_CRUD, read_CRUD, update_CRUD, delete_CRUD
 db_file_path = "subsetDB.db"  # SQLite DB 파일 경로
 record_id = 12
 delete_id = 13
-
-# Load db file
-load_database()
-
-
-# Create from CRUD
 data = (13, 1, 3, "female", 25)
-create_CRUD("subsetDB.db", data)
-
-
-# Read from CRUD
-read_CRUD(db_file_path)
-
-
-# update from CRUD
 new_data = (1, 2, "male", 35)
-update_CRUD("subsetDB.db", record_id, new_data)
 
 
-# delete from CRUD
-delete_CRUD(db_file_path, delete_id)
+
+if __name__ == "__main__":
+    load_database()
+    create_CRUD("subsetDB.db", data)
+    read_CRUD(db_file_path)
+    update_CRUD("subsetDB.db", record_id, new_data)
+    delete_CRUD(db_file_path, delete_id)

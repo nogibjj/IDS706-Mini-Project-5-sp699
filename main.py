d285ebbdc938f0748cc1cd8bdd9e36d3ec867a5e
Suim-Park <suim.park@duke.edu>
2023-10-03 23:17:49 -0400

    Update files

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from library.extract import extract_file
 from library.transform import load_file
 from library.query import create_CRUD, read_CRUD, update_CRUD, delete_CRUD
 
-csv_file_path = "Data/titanic.csv"  # CSV 파일 경로
-db_file_path = "titanic_passengersDB.db"  # SQLite DB 파일 경로
-table_name = "titanic"  # 테이블 이름
+csv_file_path = "Data/subset.csv"  # CSV 파일 경로
+db_file_path = "subsetDB.db"  # SQLite DB 파일 경로
+table_name = "subset"  # 테이블 이름
 
 # Extract .csv file
 extract_file()
@@ -15,7 +15,7 @@ extract_file()
 load_file(csv_file_path, db_file_path, table_name)
 
 # Create from CRUD
-data = (892, 1, 1, "Rose, Miss. DeWitt Bukater", "female", 19, 0, 1, 12, 100, 2, "Q")
+data = (14, 1, 3, "female", 25)
 create_CRUD(data)
 
 # Read from CRUD
@@ -24,7 +24,7 @@ print(read_CRUD())
 
 
 # update from CRUD
-update_CRUD(892, "Age", 18)
+update_CRUD(13, "age", 18)
 
 # delete from CRUD
-delete_CRUD(892)
+delete_CRUD(13)

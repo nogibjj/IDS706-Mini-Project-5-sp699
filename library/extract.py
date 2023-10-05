2fd3cb71455bd61891c28800354307f8690e1a17
Suim <143478016+suim-park@users.noreply.github.com>
2023-10-05 15:05:24 +0000

    Update library/extract.py

---
+++ library/extract.py
@@ -0,0 +1,9 @@
+# Extract csv file through link
+import requests
+
+def extract(url="https://github.com/suim-park/Mini-Project-5/blob/main/Data/subset.csv", 
+            file_path="Data/subset.csv"):
+    with requests.get(url) as r:
+        with open(file_path, 'wb') as f:
+            f.write(r.content)
+    return file_path
